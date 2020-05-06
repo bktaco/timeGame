61d786ddf423483ba00c09954aa18e17a277b5d5
Jamie <Jamieadamjordan@gmail.com>
2020-05-06 09:27:48 -0700

    Movement uses a Vector2

--- Assets/input.cs
+++ Assets/input.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class input : MonoBehaviour
 {
     List<float> nums = new List<float>();
+    List<Vector2> movement = new List<Vector2>();
     private Rigidbody2D rb;
-    private float moveSpeed = 10f;
+    private float moveSpeed = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +20,8 @@ public class input : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.D) || (Input.GetKeyDown(KeyCode.A)))
         {
-            nums.Add(Input.GetAxisRaw("Horizontal"));
+            var x = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            movement.Add(x);
         }
     }
 
@@ -27,17 +29,18 @@ public class input : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            StartCoroutine("Playback");
+            StartCoroutine("PlayBack");
         }
     }
 
-    IEnumerator Playback()
+    IEnumerator PlayBack()
     {
-        for (int i = 0; i < nums.Count; i += 1)
+        for (int i = 0; i < movement.Count; i++)
         {
-            rb.velocity = new Vector2(moveSpeed * nums[i], rb.position.y);
+            Debug.Log(movement[i].x);
+            rb.velocity = new Vector2(moveSpeed * movement[i].x, movement[i].y);
             yield return new WaitForSeconds(1);
         }
-        nums = new List<float>();
+
     }
 }
